fecacbcdbc190dc1cc9be5dc3df79fb0ccfef620
rafarillo <58669127+rafarillo@users.noreply.github.com>
2025-11-16 20:01:19 -0300

    Animated Simplex noise

--- Scripts/PerlinNoise2D.gd
+++ Scripts/PerlinNoise2D.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 @export var sprite: Sprite2D
 @export var _increment:float = 0.1
+@export var _timeSpeed := 1.0
 
 var screenSize: Vector2
 var noise := FastNoiseLite.new()
+var _time := 0.0
 
 func _ready() -> void:
 	screenSize = DisplayServer.window_get_size()
@@ -18,16 +20,16 @@ func _draw() -> void:
 func DrawMain() -> void:
 	while true:
 		await get_tree().create_timer(0.01).timeout
+		_time += _timeSpeed
 		queue_redraw()
 
 func DrawPerlinNoise():
 	var img := sprite.texture.get_image()
-	
 	var xoff := 0.0
 	for i in range(0, img.get_width()):
 		var yoff := 0.0
 		for j in range(0, img.get_height()):
-			var randomNoise: float = remap(noise.get_noise_2d(xoff, yoff), -1.0, 1.0, 0.0, 1.0)
+			var randomNoise: float = remap(noise.get_noise_3d(xoff, yoff, _time), -1.0, 1.0, 0.0, 1.0)
 			var color:Color = Color(randomNoise, randomNoise, randomNoise)
 			img.set_pixel(i, j, color)
 			# draw_circle(position, 1.0, color)
